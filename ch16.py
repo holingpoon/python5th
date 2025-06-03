92881d387059e4a44c1b7fe97b1c942b7aa37dd8
holingpoon <holingpoon@nypl.org>
2025-06-02 20:50:04 -0400

    Finishing Chapter 16.

--- ch16.py
+++ ch16.py
@@ -9,8 +9,8 @@ input("?")
 print("Opening the file...")
 target = open(filename, 'w')
 
-print("Truncating the file.  Goodbye!")
-target.truncate()
+# print("Truncating the file.  Goodbye!")
+# target.truncate()
 
 print("Now I'm going to ask you for three lines.")
 
@@ -20,12 +20,7 @@ line3 = input("line 3: ")
 
 print("I'm going to write these to the file.")
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(line1 + "\n" + line2 + "\n" + line3 + "\n")
 
 print("And finally, we close it.")
 target.close()
