c521fe6abb1a10511fc7b283272864faf4c0de7c
holingpoon <holingpoon@gmail.com>
2025-12-20 11:38:31 -0500

    chapter 21

---
+++ ch21.py
@@ -0,0 +1,34 @@
+def add(a, b):
+  print(f"ADDING {a} + {b}")
+  return a + b
+
+def subtract(a, b):
+  print(f"SUBTRACTING {a} - {b}")
+  return a - b
+
+def multiply(a, b):
+  print(f"MULTIPLYING {a} * {b}")
+  return a * b
+
+def divide(a, b):
+  print(f"DIVIDING {a} / {b}")
+  return a / b
+
+print("Let's do some math with just functions!")
+print("Enter a value:")
+a = int(input())
+
+age = add(a,5)
+height = subtract(a, 4)
+weight = multiply(a, 2)
+iq = divide(a, 2)
+
+print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
+
+# A puzzle for the extra credit, type it in anyway.
+print("Here is a puzzle.")
+
+# 35 + (74 - (180 * (50 / 2)))
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print("That becomes: ", what, "Can you do it by hand?")
