e7a94de509d6e04f0cc70cddbd0e610e847026b6
holingpoon <holingpoon@gmail.com>
2025-12-19 21:36:52 -0500

    chapter 18

---
+++ ch18_samples.py
@@ -0,0 +1,16 @@
+def do_nothing():
+  print("Do nothing")
+
+do_nothing()
+
+def do_something():
+  print("I did something!")
+
+do_something()
+
+def do_more_things(a, b):
+    a = "hello"
+    b = 1
+    print("A IS", a, "B IS", b)
+
+do_more_things("goodbye", 2)
