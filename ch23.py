7343452494e2fa6f2040581d8335a6bfed9996cb
holingpoon <holingpoon@gmail.com>
2025-12-26 19:00:50 -0500

    chapter 23

--- ch23.py
+++ ch23.py
@@ -1,15 +1,30 @@
 fruit = [
-  ['Apples', 12, 'AAA']
+  ['Apples', 12, 'AAA'],
+  ['Oranges', 1, 'B'],
+  ['Pears', 2, 'A'],
+  ['Grapes', 14, 'UR']
 ]
 
 cars = [
-  ['Cadillac', ['Black', 'Big', 34500]]
+  ['Cadillac', ['Black', 'Big', 34500]],
+  ['Corvette', ['Red', 'Little', 1000000]]
 ]
 
 languages = [
-  ['Python', ['Slow', ['Terrible', 'Mush']]]
+  ['Python', ['Slow', ['Terrible', 'Mush']]],
+  ['JavaSCript', ['Moderate', ['Alright', 'Bizarre']]],
+  ['Perl6', ['Moderate', ['Fun', 'Weird']]],
+  ['C', ['Fast', ['Annoying', 'Dangerous']]],
+  ['Forth', ['Fast', ['Fun', 'Difficult']]],
 ]
 
-print(f"Fruits: {fruit}")
-print(f"Cars: {cars}")
-print(f"Languages: {languages}")
+# fruits challenge
+print(f"12: {fruit[0][1]}")
+print(f"AAA: {fruit[0][2]}")
+print(f"2: {fruit[2][1]}")
+print(f"Oranges: {fruit[1][0]}")
+print(f"Grapes: {fruit[3][0]}")
+print(f"14: {fruit[3][1]}")
+print(f"Apples: {fruit[0][0]}")
+
+
